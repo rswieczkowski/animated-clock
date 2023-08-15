7dc354263d369ff778bfe0df7e82bf14a8e6ec90
Rafał Świeczkowski <rswieczkowski@leonsoftware.com>
2023-08-15 22:25:28 +0200

    Add possibility of changing color

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+const canvas = document.getElementById('canvas');
+const faceColor = document.getElementById('face-color');
+const borderColor = document.getElementById('border-color');
+const lineColor = document.getElementById('line-color');
+const largeHandColor = document.getElementById('large-hand-color');
+const secondHandColor = document.getElementById('second-hand-color');
+
 function clock() {
   const now = new Date();
-  const canvas = document.getElementById('canvas');
   const ctx = canvas.getContext('2d');
 
   // Setup canvas
@@ -20,7 +26,8 @@ function clock() {
   ctx.save();
   ctx.beginPath();
   ctx.lineWidth = 14;
-  ctx.strokeStyle = '#800000';
+  ctx.strokeStyle = borderColor.value;
+  ctx.fillStyle = faceColor.value;
   ctx.arc(0, 0, 142, 0, Math.PI * 2, true);
   ctx.stroke();
   ctx.fill();
@@ -28,23 +35,25 @@ function clock() {
 
   // Draw hours
   ctx.save();
+  ctx.strokeStyle = lineColor.value;
   for (let i = 0; i < 12; i++) {
     ctx.beginPath();
     ctx.rotate(Math.PI / 6);
-    ctx.moveTo(100, 0);
-    ctx.lineTo(120, 0);
+    ctx.moveTo(110, 0);
+    ctx.lineTo(130, 0);
     ctx.stroke();
   }
   ctx.restore();
 
   // Draw minutes
   ctx.save();
+  ctx.strokeStyle = lineColor.value;
   ctx.lineWidth = 4;
   for (let i = 0; i < 60; i++) {
     if (i % 5 !== 0) {
       ctx.beginPath();
-      ctx.moveTo(117, 0);
-      ctx.lineTo(120, 0);
+      ctx.moveTo(127, 0);
+      ctx.lineTo(130, 0);
       ctx.stroke();
     }
     ctx.rotate(Math.PI / 30);
@@ -63,34 +72,34 @@ function clock() {
   ctx.rotate(
     (Math.PI / 6) * hr + (Math.PI / 360) * min + (Math.PI / 21600) * sec
   );
-  ctx.strokeStyle = '#800000';
+  ctx.strokeStyle = largeHandColor.value;
   ctx.lineWidth = 10;
   ctx.beginPath();
   ctx.moveTo(-20, 0);
-  ctx.lineTo(80, 0);
+  ctx.lineTo(90, 0);
   ctx.stroke();
   ctx.restore();
 
   // Draw minute hand
   ctx.save();
   ctx.rotate((Math.PI / 30) * min + (Math.PI / 1800) * sec);
-  ctx.strokeStyle = '#800000';
+  ctx.strokeStyle = largeHandColor.value;
   ctx.lineWidth = 8;
   ctx.beginPath();
   ctx.moveTo(-28, 0);
-  ctx.lineTo(112, 0);
+  ctx.lineTo(122, 0);
   ctx.stroke();
   ctx.restore();
 
   // Draw second hand
   ctx.save();
   ctx.rotate(sec * (Math.PI / 30));
-  ctx.strokeStyle = '#FF7F50';
-  ctx.fillStyle = '#FF7F50';
+  ctx.strokeStyle = secondHandColor.value;
+  ctx.fillStyle = secondHandColor.value;
   ctx.lineWidth = 5;
   ctx.beginPath();
   ctx.moveTo(-30, 0);
-  ctx.lineTo(100, 0);
+  ctx.lineTo(110, 0);
   ctx.stroke();
   ctx.beginPath();
   ctx.arc(0, 0, 5, 0, Math.PI * 2, true);
@@ -103,3 +112,11 @@ function clock() {
 }
 
 requestAnimationFrame(clock);
+
+document.getElementById('save-btn').addEventListener('click', () => {
+  const dataURL = canvas.toDataURL('image/png');
+  const link = document.createElement('a');
+  link.download = 'clock.png';
+  link.href = dataURL;
+  link.click();
+});
